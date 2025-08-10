1d508e3a7ca6cc7a14c63d2dd7905059c2ae0c60
jhasbdmf <oshtyria@uni-osnabrueck.de>
2025-08-10 14:08:27 +0200

    feat: RMSprop runs in parallel to SGD

--- neural_n_grams.py
+++ neural_n_grams.py
@@ -1,9 +1,7 @@
 from utilities import read_file_from
 import numpy as np
-import torch
-import torch.nn.functional as F
-import torch.optim as optim
 from numpy.random import default_rng
+import random
 
 class Token_Translator:
     def __init__(self, vocab: list):
@@ -17,8 +15,8 @@ class Token_Translator:
         result = []
         for i in list_to_decode:
             # if i is a torch tensor, extract its value
-            if isinstance(i, torch.Tensor):
-                i = i.item()   # now a Python int
+            #if isinstance(i, torch.Tensor):
+            #    i = i.item()   # now a Python int
             result.append(self.index_to_symbol[i])
         return result
 
@@ -28,7 +26,7 @@ class Neural_n_gram_model:
         rng = np.random.default_rng(seed=42)  
         np_table = rng.normal(
             loc=0.0,          # mean
-            scale=0.1,        # standard deviation
+            scale=0.2,        # standard deviation
             size=(vocabulary_size, vocabulary_size)
         ).astype(np.float32)
         #np_table = 0,3 * rng.standard_normal((vocabulary_size, vocabulary_size)).astype(np.float32)
@@ -40,12 +38,16 @@ class Neural_n_gram_model:
     #only logit of the target class needs to be normalized
     #to compute cross entropy of the respective prediction
     def _get_cross_entropy (self, logits, target_index):
-        softmax_denominator = np.sum(np.exp(logits))
+        softmax_denominator = np.sum(np.exp(logits)) + 1e-8
         return -logits[target_index] + np.log(softmax_denominator)
     
     def _get_normalized_logits(self, logits):
         softmax_denominator = np.sum(np.exp(logits))
         return np.exp(logits) / softmax_denominator
+    
+    def _get_normalized_logits_with_softmax_denom(self, logits):
+        softmax_denominator = np.sum(np.exp(logits))
+        return np.exp(logits) / softmax_denominator, softmax_denominator
     
     def forward(self, token_idx: int, target_index: int=None):
    
@@ -61,10 +63,17 @@ class Neural_n_gram_model:
         #just return the cross-entropy loss gradient
         #to accelerate gradient descent
         else:
-            CEL_gradient = self._get_normalized_logits(logits)
-            true_class_probability = CEL_gradient[target_index] 
-            CEL_value = -np.log(true_class_probability)
-            CEL_gradient[target_index] -= 1
+            #CEL_gradient = self._get_normalized_logits(logits)
+            normalized_logits, softmax_denom = self._get_normalized_logits_with_softmax_denom(logits)
+
+            CEL_value = -logits[target_index] + np.log(softmax_denom)
+
+            normalized_logits[target_index] -= 1
+            CEL_gradient = normalized_logits
+
+            #predicted_true_class_probability = normalized_logits[target_index] 
+            #CEL_value = -np.log(predicted_true_class_probability)
+            
             return CEL_value, CEL_gradient
 
    
@@ -101,45 +110,91 @@ preceding_tokens = encoded_tokens[:-1]
 subsequent_tokens = encoded_tokens[1:]
 
 
-neural_n_gram_model = Neural_n_gram_model(vocabulary_size)
+neural_n_gram_model_SGD = Neural_n_gram_model(vocabulary_size)
+neural_n_gram_model_RMSprop = Neural_n_gram_model(vocabulary_size)
 
 
 
-
-LEARNING_RATE = 0.4
+SGD_LEARNING_RATE = 0.5
+RMS_PROP_INITIAL_LR = 0.3
 LEARNING_RATE_MULTIPLIER_PER_EPOCH = 0.95
-N_EPOCHS = 50
+N_EPOCHS = 30
 
-
+RMS_PROP_RHO = 0.9
+#RMS_PROP_EPSILON = 0.1e-7
 
 
 print (f"BPE k = {K}")
-print (f"learning rate = {LEARNING_RATE}")
+print (f"learning rate = {SGD_LEARNING_RATE}")
 print (f"learning rate decay per epoch = {LEARNING_RATE_MULTIPLIER_PER_EPOCH}")
 print (f"n_epochs = {N_EPOCHS}")
 print ("_" * 50)
 
 
+loss_history_SGD = []
+loss_history_RMSprop = []
+
+RMSprop_running_square_gradient_avg = np.zeros_like(neural_n_gram_model_RMSprop.token_embedding_table)
+
 for epoch_index in range(1, N_EPOCHS + 1):
 
-    print (f"current epoch number is {epoch_index}")
-    print (f"current learning rate = {LEARNING_RATE}")
-    total_loss = 0
-    for x,y in zip(preceding_tokens, subsequent_tokens):
-        #get the CEL gradient from the forward pass directly
-        loss, loss_gradient = neural_n_gram_model.forward(x, y)
-        #do gradient step immediately
-        neural_n_gram_model.token_embedding_table[x] -= LEARNING_RATE*loss_gradient
-        total_loss += loss
+    print (f"Epoch {epoch_index}/{N_EPOCHS}")
+    print (f"current SGD learning rate = {SGD_LEARNING_RATE}")
+
     
-    LEARNING_RATE *= LEARNING_RATE_MULTIPLIER_PER_EPOCH
+   
+    total_RMS_prop_lr = 0.0
+    total_loss_SGD = 0
+    total_loss_RMSprop = 0
+    
 
-    total_loss /= len(preceding_tokens)
-    print (f"average loss in epoch {epoch_index} is {total_loss}")
+    token_pairs = list(zip(preceding_tokens, subsequent_tokens))
+    random.shuffle(token_pairs) 
+    #for x,y in zip(preceding_tokens, subsequent_tokens):
+    for x,y in token_pairs:
 
-    input_sequence = [78]
-    input_sequence = neural_n_gram_model.generate_new_tokens(input_sequence, 50)
-    decoded_input_sequence = token_translator.decode_list(input_sequence)
-    print ("".join(decoded_input_sequence).replace("</w>", " "))
+        #get the CEL gradient from the forward pass directly
+        loss_SGD, loss_gradient_SGD = neural_n_gram_model_SGD.forward(x, y)
+        loss_RMSprop, loss_gradient_RMSprop = neural_n_gram_model_RMSprop.forward(x, y)
+
+
+        #compute running gradient avg for RMSprop
+        RMSprop_running_square_gradient_avg[x] *=  RMS_PROP_RHO
+        RMSprop_running_square_gradient_avg[x] += (loss_gradient_RMSprop ** 2) * (1-RMS_PROP_RHO)
+
+        #do gradient step immediately
+        neural_n_gram_model_SGD.token_embedding_table[x] -= SGD_LEARNING_RATE*loss_gradient_SGD
+
+        RMSprop_denominator = np.sqrt(RMSprop_running_square_gradient_avg[x] + 0.1e-6)
+        RMSprop_lr = RMS_PROP_INITIAL_LR / RMSprop_denominator
+        total_RMS_prop_lr += RMSprop_lr.mean()
+        neural_n_gram_model_RMSprop.token_embedding_table[x] -=  RMSprop_lr * loss_gradient_RMSprop
+
+        total_loss_SGD += loss_SGD
+        total_loss_RMSprop += loss_RMSprop
+    
+    SGD_LEARNING_RATE *= LEARNING_RATE_MULTIPLIER_PER_EPOCH
+    total_RMS_prop_lr /= len(preceding_tokens)
+    print (f"current avg RMSprop learning rate = {total_RMS_prop_lr}")
+
+    total_loss_SGD /= len(preceding_tokens)
+    print (f"average SGD loss in epoch {epoch_index} is {total_loss_SGD}")
+
+    total_loss_RMSprop /= len(preceding_tokens)
+    print (f"average RMSprop loss in epoch {epoch_index} is {total_loss_RMSprop}")
     print ("_" * 50)
+
+input_sequence_SGD = [78]
+input_sequence_SGD = neural_n_gram_model_SGD.generate_new_tokens(input_sequence_SGD, 200)
+decoded_input_sequence_SGD = token_translator.decode_list(input_sequence_SGD)
+print ("SGD text")
+print ("".join(decoded_input_sequence_SGD).replace("</w>", " "))
+print ("_" * 50)
+
+input_sequence_RMSprop = [78]
+input_sequence_RMSprop = neural_n_gram_model_SGD.generate_new_tokens(input_sequence_RMSprop, 200)
+decoded_input_sequence_RMSprop = token_translator.decode_list(input_sequence_RMSprop)
+print ("SGD text")
+print ("".join(decoded_input_sequence_RMSprop).replace("</w>", " "))
+print ("_" * 50)
     
