d623e963f818b06a349bd6db9d9c94499437a479
jhasbdmf <oshtyria@uni-osnabrueck.de>
2025-08-31 12:25:49 +0200

    refactor: plotting parametrized wrt K

--- simple_n_grams.py
+++ simple_n_grams.py
@@ -144,7 +144,7 @@ def get_perplexity_score_of_via(corpus_segment: str,
  
     return perplexity
 
-def plot_perplexity (perplexity_scores1: list, perplexity_scores2: list, k:int):
+def plot_perplexity (perplexity_scores1: list, perplexity_scores2: list, k1:int, k2: int):
     # Indices
     #indices1 = range(len(perplexity_scores_K_2000))  
     indices1 = np.arange(len(perplexity_scores1)) + 1
@@ -153,12 +153,12 @@ def plot_perplexity (perplexity_scores1: list, perplexity_scores2: list, k:int):
     #indices2 = range(len(val_loss_history)) 
 
     # Plot both
-    plt.plot(indices1, perplexity_scores1, marker='o', linestyle='-', label='vocab size k = 400')
-    plt.plot(indices2, perplexity_scores2, marker='s', linestyle='--', label='vocab size k = 2000')
+    plt.plot(indices1, perplexity_scores1, marker='o', linestyle='-', label=f'vocab size k = {k1}')
+    plt.plot(indices2, perplexity_scores2, marker='s', linestyle='--', label=f'vocab size k = {k2}')
 
     plt.xlabel('Magnitude of n in n_grams')
     plt.ylabel('Perplexity on test set')
-    plt.title(f'Perplexity scores of simple n_grams of varying sizes on test set with vocab size k = {k}')
+    plt.title(f'Perplexity scores of simple n_grams of varying sizes on test set with vocab size k = {k1, k2}')
     plt.legend()
     plt.grid(True)
 
@@ -217,7 +217,7 @@ for i in range(1, MAX_N_GRAM_LENGTH + 1):
     print (f"For vocab size k = {K} and n = {i} perplexity on {evaluation_set} set is equal to {perplexity}")
 
 
-plot_perplexity (perplexity_scores_K_400, perplexity_scores_K_2000, K)
+plot_perplexity (perplexity_scores_K_400, perplexity_scores_K_2000, 400, 2000)
 
 
 go_into_generation_mode (K)
